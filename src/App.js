6d73ae92df147260c97ba341e00728cc03c509b3
xMartin <xmartin@siarp.de>
2017-04-04 18:12:24 +0200

    Aufgabe 2

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+  test = 'bla';
+
+  render() {
+    return (
+      <div className="App">
+        {this.test}
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
